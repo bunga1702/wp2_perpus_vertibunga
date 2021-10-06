3f810a262a8940db512b250f27cd996b0d33925f
bunga1702 <bunga1702@gmail.com>
2021-10-06 16:04:45 +0700

    Tugas pertemuan 4

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -1,15 +1,16 @@
 <html>
-  
+
   <head>
-    <title>Tampil Data Matakuliah</title>
+    <title>Form Input Matakuliah</title>
   </head>
 
   <body>
     <center>
+      <form action="<?= base_url('Matakuliah/cetak');?>" method="post">
       <table>
         <tr>
           <th colspan="3">
-            Tampil Data Mata Kuliah
+            Form Input Mata Kuliah
           </th>
         </tr>
 
@@ -23,7 +24,7 @@
           <th>Kode MTK</th>
           <th>:</th>
           <td>
-            <?= $kode; ?>
+            <input type="text" name="kode" id="kode">
           </td>
         </tr>
 
@@ -31,7 +32,7 @@
           <th>Nama MTK</th>
           <th>:</th>
           <td>
-            <?= $nama; ?>
+            <input type="text" name="nama" id="nama">
           </td>
         </tr>
 
@@ -39,16 +40,22 @@
           <th>SKS</th>
           <th>:</th>
           <td>
-            <?= $sks; ?>
+            <select name="sks" id="sks">
+              <option value="">Pilih SKS</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
           </td>
         </tr>
 
         <tr>
           <td colspan="3" align="center">
-            <a href="<?= base_url('Matakuliah');?>">Kembali</a>
+            <input type="submit" value="Submit">
           </td>
         </tr>
       </table>
+      </form>
     </center>
   </body>
 
